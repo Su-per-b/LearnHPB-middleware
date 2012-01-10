c54c9cd8ebba42d5cbdf32ea865223fe0cbde7bb
rajdye <raj@pcdigi.com>
2012-01-10 01:05:05 -0800

    .

--- assets/scripts/test_fmu.py
+++ assets/scripts/test_fmu.py
@@ -3,5 +3,5 @@ from jmodelica.fmi import FMUModel
 myModel=FMUModel('/var/tmp/JModelica/Python/src/jmodelica/examples/files/FMUs/bouncingBall.fmu')
 myModel.simulate()
 
-myModel2=FMUModel('/home/ubuntu/testFMI.fmu')
+myModel2=FMUModel('/home/ubuntu/scripts/testFMI.fmu')
 myModel2.simulate()
