85686318eeca9814d46e585a12b985ec14d2ca2d
rajdye <raj@pcdigi.com>
2012-02-20 03:46:32 -0800

    added scripts

---
+++ assets/scripts/windows/start_straylight.bat
@@ -0,0 +1,7 @@
+
+
+
+cd C:\straylight\eclipseWorkspace\StrayLight3\jspserver3
+start mvn exec:java
+cd C:\straylight\eclipseWorkspace\StrayLight3\socketserver
+start mvn exec:java
---
+++ assets/scripts/windows/update_straylight.bat
@@ -0,0 +1,5 @@
+cd C:\straylight
+git pull 
+
+cd C:\straylight\eclipseWorkspace\StrayLight3
+mvn clean install
