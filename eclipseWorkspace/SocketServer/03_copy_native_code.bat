26c9c500d4aa26ac8d41db7c067d841e2e26089b
rajdye <raj@pcdigi.com>
2015-05-19 00:24:43 -0700

    .

--- eclipseWorkspace/SocketServer/03_copy_native_code.bat
+++ eclipseWorkspace/SocketServer/03_copy_native_code.bat
@@ -7,7 +7,7 @@ mkdir ..\FMUwrapper\native_code
 copy /y ..\..\visualStudioWorkspace\bin\Debug\expat.dll ..\FMUwrapper\native_code\expat.dll
 copy /y ..\..\visualStudioWorkspace\bin\Debug\FMUwrapper.dll ..\FMUwrapper\native_code\FMUwrapper.dll
 
-xcopy /y /e /v /i ..\..\assets\FMUs\LearnGB_0v4_02_VAVReheat_ClosedLoop_edit2 ..\FMUwrapper\native_code\fmu
+xcopy /y /e /v /i ..\..\assets\FMUs\LearnGB_0v4_02_VAVReheat_ClosedLoop_edit3_summer ..\FMUwrapper\native_code\fmu
 
 xcopy /y /e /v /i ..\FMUwrapper\native_code published\native_code
 xcopy /y /e /v /i ..\FMUwrapper\native_code .\native_code
